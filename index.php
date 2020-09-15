94e9861fd04e6804c137b846af31e5950d9467cd
dnabilaa <70683312+dnabilaa@users.noreply.github.com>
2020-09-15 11:29:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<html>
+<h1>Praktikum 1 piramida</h1> 
+<p>Dinda Nabila Amartha (6701194064)</p>
 <?php
-
-?>
+for ($i=1; $i<=10; $i++){
+    echo "<center>";
+    for ($d=$i; $d<=10; $d++) {
+    echo "*";
+    }
+    echo "<br />";
+    }
+    ?>
+</html>
